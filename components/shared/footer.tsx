00fba224820266c9fd2362aff8a6c8586b3fffa1
Joss Bleach <jossbleach@josss-mbp.home>
2024-01-25 17:47:27 +0000

    Added footer.

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,3 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  const date = new Date();
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/SVG/sonix-logo.svg"
+            alt="Sonix logo"
+            width={38}
+            height={38}
+          />
+        </Link>
+        <p>&copy; Sonix {date.getFullYear()}. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
