f4c5b425f5456ec39e2b2a78ca2f69d47cc93c90
Joss Bleach <jossbleach@josss-mbp.home>
2024-01-26 16:43:55 +0000

    Finished with hero section + prep for rest of home page.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,50 @@
+import Link from "next/link";
+import Image from "next/image";
+
+// Components
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
     <main>
-      <h1 className="text-4xl">Sonix</h1>
+      <>
+        <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+          <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+            <div className="flex flex-col justify-center gap-8">
+              <h1 className="h1-bold">
+                Create share, and experience extraordinary music events
+              </h1>
+              <p className="p-regular-20 md:p-regular-24">
+                View our range of events to sell and secure tickets to diverse
+                music experiences worldwide, all in one place.
+              </p>
+              <Button className="w-full rounded sm:w-fit" asChild size="lg">
+                <Link href="#events">Explore</Link>
+              </Button>
+            </div>
+            <Image
+              src="/assets/images/hero.png"
+              className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+              alt="A girl dancing."
+              width={1000}
+              height={1000}
+              priority
+            />
+          </div>
+        </section>
+        <section
+          id="events"
+          className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+        >
+          <h2 className="h2-bold">
+            Trusted by <br />
+            the best music events
+          </h2>
+          <div className="flex w-full flex-col gap-5 md:flex-row">
+            Search + Categories
+          </div>
+        </section>
+      </>
     </main>
   );
 };
