72d14e9e106a28e6b28b2b3e1867833327a25e43
Joss Bleach <jossbleach@josss-mbp.home>
2024-01-29 17:24:26 +0000

    Adding event page.

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,13 @@
+const CreateEventPage = () => {
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Add your event
+        </h3>
+      </section>
+    </>
+  );
+};
+
+export default CreateEventPage;
